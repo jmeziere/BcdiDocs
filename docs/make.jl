12f25849f00485e56bb00d606b4ed0d49cc7c18d
jmeziere <jmeziere@gmail.com>
2024-08-23 21:15:17 -0600

    add refs

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,60 @@
-using Documenter, BcdiTrad, BcdiStrain, BcdiMeso
+using Documenter, DocumenterCitations, BcdiTrad, BcdiStrain, BcdiMeso
+
+inEntry = false
+newEntry = true
+entryName = ""
+entry = String[]
+entryDict =  Dict{String,Vector{String}}()
+
+inBibFiles = [
+    "docs/src/BcdiCore.jl/docs/src/refs.bib"
+    "docs/src/BcdiTrad.jl/docs/src/refs.bib"
+    "docs/src/BcdiStrain.jl/docs/src/refs.bib"
+    "docs/src/BcdiMeso.jl/docs/src/refs.bib"
+]
+outBibFile = "docs/src/refs.bib"
+for filename in inBibFiles
+    open(filename,"r") do fin
+        while !eof(fin)
+            global inEntry
+            global newEntry
+            global entryName
+            global entry
+            line = readline(fin)
+            if inEntry == false && occursin("@", line)
+                inEntry = true
+                startInd = findfirst("{",line)[1]+1
+                endInd = findfirst(",",line)[1]-1
+                entryName = line[startInd:endInd]
+                if entryName in keys(entryDict)
+                    newEntry = false
+                else
+                    newEntry = true
+                end
+                entry = String[line]
+            elseif inEntry == true && newEntry
+                if strip(line, [' ','\n']) == "}"
+                   push!(entry, line)
+                   entryDict[entryName] = entry
+                   inEntry = false
+                else
+                   push!(entry, line)
+                end
+            end
+        end
+    end
+end
+
+open(outBibFile,"w") do fout
+    for k in keys(entryDict)
+        for line in entryDict[k]
+            println(fout, line)
+        end
+        println(fout, "")
+    end
+end
+
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"))
 
 makedocs(
     sitename="BcdiDocs",
@@ -36,7 +92,8 @@ makedocs(
         "src/BcdiTrad.jl"=>Documenter.Remotes.GitHub("byu-cxi", "BcdiTrad.jl.git"),
         "src/BcdiStrain.jl"=>Documenter.Remotes.GitHub("byu-cxi", "BcdiStrain.jl.git"),
         "src/BcdiMeso.jl"=>Documenter.Remotes.GitHub("byu-cxi", "BcdiMeso.jl.git")
-    )
+    ),
+    plugins = [bib]
 )
 
 deploydocs(
